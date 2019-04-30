6b9e132bf9d8fa1f3820efcfa7d5218842ddac2b
Mar Mazuelas <mm.zlsmng@gmail.com>
2019-04-30 12:28:05 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+#Primer Ejercicio de Evaluación de JavaScript
+Carpeta css
+Carpeta js
+carpeta img
+pasarlo a Git
+pasarlo a github y pasarle la url
+Bootstrap
+Instalar SourceTree, configurarlo y acceder con BitBucket
---
+++ js/codigo.js
@@ -0,0 +1,47 @@
+function copyAno(){
+    document.getElementById("copyano").innerHTML = new Date().getFullYear();
+}
+
+function muestraFecha(){
+    var miHora = new Date();
+    var diaSemana = '';
+
+    switch (new Date().getDay()){
+        case 0:
+            diaSemana = 'Domingo';
+            break;
+        case 1:
+            diaSemana = 'Lunes';
+            break;
+        case 2:
+            diaSemana = 'Martes';
+            break;
+        case 3:
+            diaSemana = 'Miércoles';
+            break;
+        case 4:
+            diaSemana = 'Jueves';
+            break;
+        case 5:
+            diaSemana = 'Viernes';
+            break;
+        case 6:
+            diaSemana = 'Sábado';
+            break;
+    }
+
+    document.getElementById("mostrardatos").innerHTML = "RESULTADOS: Hoy es " + diaSemana + ". La hora actual es " +
+            miHora.getHours() + ":" + miHora.getMinutes() + ".";
+}
+
+function voltear(){
+    var numero = window.prompt("Por favor, introduce un número:");
+
+    if (numero!=null){
+        var volteado = numero.split("");
+        volteado.reverse();
+        alert("El numero volteado es " + volteado.join("") + ".");
+    }else{
+        alert("El número introducido no es correcto.");
+    }
+}
